eca9f7eee835cff11f886122f9c8f1cb7752bfe6
LetDevOwl <134674322+LetDevOwl@users.noreply.github.com>
2023-07-08 00:32:46 +0900

    [ADD] 23/07/08 Quiz Program Update
    1. Array -> list
    2. 공통 부분 그룹핑(Grouping)

--- src/com/kh/mini/quiz/Quiz2.java
+++ src/com/kh/mini/quiz/Quiz2.java
@@ -4,26 +4,43 @@ public class Quiz2 {
 	private String quizContent;
 	private String choiceArrs;
 	private int answerSheet;
-	private int answer;
 	// 전체퀴즈 저장
 	// 1 ~ 60 4지선다 (1 ~ 20 : html, 21 ~ 40 : oracle, 41 ~ 60 : java)
 	// 61 ~ 120 O/X (61 ~ 80 : html, 81 ~ 100 : oracle, 101 ~ 120 : java)
 
-	public Quiz2() {}
+	public Quiz2() {
+	}
+
 	public Quiz2(String quizContent, String choiceArrs, int answerSheet) {
-		this.quizContent = quizContent;
-		this.choiceArrs = choiceArrs;
-		this.answerSheet = answerSheet;
+		this.setQuizContent(quizContent);
+		this.setChoiceArrs(choiceArrs);
+		this.setAnswerSheet(answerSheet);
 	}
-	public Quiz2(int answer) {
-		this.answer = answer;
-	}
-	
+
+	// getter
 	public String getQuizContent() {
 		return quizContent;
 	}
 
+	public String getChoiceArrs() {
+		return choiceArrs;
+	}
+
+	public int getAnswerSheet() {
+		return answerSheet;
+	}
+
+	// setter
 	public void setQuizContent(String quizContent) {
 		this.quizContent = quizContent;
 	}
+
+	public void setChoiceArrs(String choiceArrs) {
+		this.choiceArrs = choiceArrs;
+	}
+
+	public void setAnswerSheet(int answerSheet) {
+		this.answerSheet = answerSheet;
+	}
+
 }
--- src/com/kh/mini/quiz/QuizFunction.java
+++ src/com/kh/mini/quiz/QuizFunction.java
@@ -7,47 +7,157 @@ import java.util.Scanner;
 
 public class QuizFunction {
 
+	Scanner sc = new Scanner(System.in);
+	Random rand = new Random();
+	Quiz2 quiz2 = new Quiz2();
+	
 	private List<Quiz2> qList;
 	private int[] answer;
+//	private List<Quiz2> userAnswer;
 	private int[] randQuestion; // 랜덤 5개 퀴즈 선택
+//	private List<randQuestion> randQ;
 
 	public QuizFunction() {
 		qList = new ArrayList<Quiz2>();
+		// html 4지선다 문제
 		qList.add(new Quiz2("4지선다 html문제1", "1.a 2.b 3.c 4.d", 1));
-		qList.add(new Quiz2());
-		qList.add(new Quiz2());
-		qList.add(new Quiz2());
-		qList.add(new Quiz2());
-		qList.add(new Quiz2());
-		qList.add(new Quiz2());
-		qList.add(new Quiz2());
-		qList.add(new Quiz2());
-		qList.add(new Quiz2());
-		qList.add(new Quiz2());
-		qList.add(new Quiz2());
-		qList.add(new Quiz2());
-		qList.add(new Quiz2());
-		qList.add(new Quiz2());
-		qList.add(new Quiz2());
+		qList.add(new Quiz2("4지선다 html문제2", "1.a 2.b 3.c 4.d", 1));
+		qList.add(new Quiz2("4지선다 html문제3", "1.a 2.b 3.c 4.d", 1));
+		qList.add(new Quiz2("4지선다 html문제4", "1.a 2.b 3.c 4.d", 1));
+		qList.add(new Quiz2("4지선다 html문제5", "1.a 2.b 3.c 4.d", 1));
+		qList.add(new Quiz2("4지선다 html문제6", "1.a 2.b 3.c 4.d", 1));
+		qList.add(new Quiz2("4지선다 html문제7", "1.a 2.b 3.c 4.d", 1));
+		qList.add(new Quiz2("4지선다 html문제8", "1.a 2.b 3.c 4.d", 1));
+		qList.add(new Quiz2("4지선다 html문제9", "1.a 2.b 3.c 4.d", 1));
+		qList.add(new Quiz2("4지선다 html문제10", "1.a 2.b 3.c 4.d", 1));
+		qList.add(new Quiz2("4지선다 html문제11", "1.a 2.b 3.c 4.d", 1));
+		qList.add(new Quiz2("4지선다 html문제12", "1.a 2.b 3.c 4.d", 1));
+		qList.add(new Quiz2("4지선다 html문제13", "1.a 2.b 3.c 4.d", 1));
+		qList.add(new Quiz2("4지선다 html문제14", "1.a 2.b 3.c 4.d", 1));
+		qList.add(new Quiz2("4지선다 html문제15", "1.a 2.b 3.c 4.d", 1));
+		qList.add(new Quiz2("4지선다 html문제16", "1.a 2.b 3.c 4.d", 1));
+		qList.add(new Quiz2("4지선다 html문제17", "1.a 2.b 3.c 4.d", 1));
+		qList.add(new Quiz2("4지선다 html문제18", "1.a 2.b 3.c 4.d", 1));
+		qList.add(new Quiz2("4지선다 html문제19", "1.a 2.b 3.c 4.d", 1));
+		qList.add(new Quiz2("4지선다 html문제20", "1.a 2.b 3.c 4.d", 1));
+		// oracle 4지선다 문제
+		qList.add(new Quiz2("4지선다 oracle문제1", "1.a 2.b 3.c 4.d", 2));
+		qList.add(new Quiz2("4지선다 oracle문제2", "1.a 2.b 3.c 4.d", 2));
+		qList.add(new Quiz2("4지선다 oracle문제3", "1.a 2.b 3.c 4.d", 2));
+		qList.add(new Quiz2("4지선다 oracle문제4", "1.a 2.b 3.c 4.d", 2));
+		qList.add(new Quiz2("4지선다 oracle문제5", "1.a 2.b 3.c 4.d", 2));
+		qList.add(new Quiz2("4지선다 oracle문제6", "1.a 2.b 3.c 4.d", 2));
+		qList.add(new Quiz2("4지선다 oracle문제7", "1.a 2.b 3.c 4.d", 2));
+		qList.add(new Quiz2("4지선다 oracle문제8", "1.a 2.b 3.c 4.d", 2));
+		qList.add(new Quiz2("4지선다 oracle문제9", "1.a 2.b 3.c 4.d", 2));
+		qList.add(new Quiz2("4지선다 oracle문제10", "1.a 2.b 3.c 4.d", 2));
+		qList.add(new Quiz2("4지선다 oracle문제11", "1.a 2.b 3.c 4.d", 2));
+		qList.add(new Quiz2("4지선다 oracle문제12", "1.a 2.b 3.c 4.d", 2));
+		qList.add(new Quiz2("4지선다 oracle문제13", "1.a 2.b 3.c 4.d", 2));
+		qList.add(new Quiz2("4지선다 oracle문제14", "1.a 2.b 3.c 4.d", 2));
+		qList.add(new Quiz2("4지선다 oracle문제15", "1.a 2.b 3.c 4.d", 2));
+		qList.add(new Quiz2("4지선다 oracle문제16", "1.a 2.b 3.c 4.d", 2));
+		qList.add(new Quiz2("4지선다 oracle문제17", "1.a 2.b 3.c 4.d", 2));
+		qList.add(new Quiz2("4지선다 oracle문제18", "1.a 2.b 3.c 4.d", 2));
+		qList.add(new Quiz2("4지선다 oracle문제19", "1.a 2.b 3.c 4.d", 2));
+		qList.add(new Quiz2("4지선다 oracle문제20", "1.a 2.b 3.c 4.d", 2));
+		// java 4지선다 문제
+		qList.add(new Quiz2("4지선다 java문제1", "1.a 2.b 3.c 4.d", 3));
+		qList.add(new Quiz2("4지선다 java문제2", "1.a 2.b 3.c 4.d", 3));
+		qList.add(new Quiz2("4지선다 java문제3", "1.a 2.b 3.c 4.d", 3));
+		qList.add(new Quiz2("4지선다 java문제4", "1.a 2.b 3.c 4.d", 3));
+		qList.add(new Quiz2("4지선다 java문제5", "1.a 2.b 3.c 4.d", 3));
+		qList.add(new Quiz2("4지선다 java문제6", "1.a 2.b 3.c 4.d", 3));
+		qList.add(new Quiz2("4지선다 java문제7", "1.a 2.b 3.c 4.d", 3));
+		qList.add(new Quiz2("4지선다 java문제8", "1.a 2.b 3.c 4.d", 3));
+		qList.add(new Quiz2("4지선다 java문제9", "1.a 2.b 3.c 4.d", 3));
+		qList.add(new Quiz2("4지선다 java문제10", "1.a 2.b 3.c 4.d", 3));
+		qList.add(new Quiz2("4지선다 java문제11", "1.a 2.b 3.c 4.d", 3));
+		qList.add(new Quiz2("4지선다 java문제12", "1.a 2.b 3.c 4.d", 3));
+		qList.add(new Quiz2("4지선다 java문제13", "1.a 2.b 3.c 4.d", 3));
+		qList.add(new Quiz2("4지선다 java문제14", "1.a 2.b 3.c 4.d", 3));
+		qList.add(new Quiz2("4지선다 java문제15", "1.a 2.b 3.c 4.d", 3));
+		qList.add(new Quiz2("4지선다 java문제16", "1.a 2.b 3.c 4.d", 3));
+		qList.add(new Quiz2("4지선다 java문제17", "1.a 2.b 3.c 4.d", 3));
+		qList.add(new Quiz2("4지선다 java문제18", "1.a 2.b 3.c 4.d", 3));
+		qList.add(new Quiz2("4지선다 java문제19", "1.a 2.b 3.c 4.d", 3));
+		qList.add(new Quiz2("4지선다 java문제20", "1.a 2.b 3.c 4.d", 3));
+		// html OX 문제
+		qList.add(new Quiz2("OX html문제1", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX html문제2", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX html문제3", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX html문제4", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX html문제5", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX html문제6", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX html문제7", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX html문제8", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX html문제9", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX html문제10", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX html문제11", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX html문제12", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX html문제13", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX html문제14", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX html문제15", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX html문제16", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX html문제17", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX html문제18", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX html문제19", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX html문제20", "1.O 2.X", 1));
+		// oracle OX 문제
+		qList.add(new Quiz2("OX oracle문제1", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX oracle문제2", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX oracle문제3", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX oracle문제4", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX oracle문제5", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX oracle문제6", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX oracle문제7", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX oracle문제8", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX oracle문제9", "1.O 2.X", 1));
+		qList.add(new Quiz2("OX oracle문제10", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX oracle문제11", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX oracle문제12", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX oracle문제13", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX oracle문제14", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX oracle문제15", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX oracle문제16", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX oracle문제17", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX oracle문제18", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX oracle문제19", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX oracle문제20", "1.O 2.X", 2));
+		// java OX 문제
+		qList.add(new Quiz2("OX java문제1", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX java문제2", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX java문제3", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX java문제4", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX java문제5", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX java문제6", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX java문제7", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX java문제8", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX java문제9", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX java문제10", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX java문제11", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX java문제12", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX java문제13", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX java문제14", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX java문제15", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX java문제16", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX java문제17", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX java문제18", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX java문제19", "1.O 2.X", 2));
+		qList.add(new Quiz2("OX java문제20", "1.O 2.X", 2));
 
 		randQuestion = new int[5];
 		answer = new int[5];
+
 	}
 
-	public void inputQuiz() {
-		String quizContent = "";
-		Quiz2 quiz2 = new Quiz2();
-		quiz2.setQuizContent(quizContent);
-	}
+//	public void inputQuiz() {
+//		String quizContent = "";
+//		Quiz2 quiz2 = new Quiz2();
+//		quiz2.setQuizContent(quizContent);
+//	}
+
 	public int printLanguageMenu() {
-//		메인화면
-//		======= KH 퀴즈 !!! =======
-//		1. HTML, CSS, JAVASCRIPT
-//		2. Oracle
-//		3. Java
-//		4. EXIT
-//		원하는 퀴즈를 선택해주세요 : (ex> 3 -> enter)
-		Scanner sc = new Scanner(System.in);
 		System.out.println("======= KH 퀴즈 !!! =======");
 		System.out.println("1. HTML, CSS, JAVASCRIPT");
 		System.out.println("2. Oracle");
@@ -60,13 +170,6 @@ public class QuizFunction {
 	}
 
 	public int printCategoryMenu() {
-//		문제유형 화면
-//		======= KH 퀴즈 !!! =======
-//		1. 4지선다
-//		2. O/X
-//		3. 이전화면으로…
-//		원하는 문제형식을 선택해주세요 : (ex> 2 -> enter)
-		Scanner sc = new Scanner(System.in);
 		System.out.println("======= KH 퀴즈 !!! =======");
 		System.out.println("1. 4지선다");
 		System.out.println("2. O/X");
@@ -77,12 +180,11 @@ public class QuizFunction {
 		return category;
 	}
 
+// html 4지선다
 	public void htmlChioceQuiz() {
-		Random rand = new Random();
-		Scanner sc = new Scanner(System.in);
-		Quiz quiz = new Quiz();
+		// html 4지선다 랜덤문제 인덱스값 5개 받기(0~19)
 		for (int i = 0; i < randQuestion.length; i++) {
-			randQuestion[i] = rand.nextInt(20); // html 4지선다 랜덤문제 인덱스값 5개 받기(0~19)
+			randQuestion[i] = rand.nextInt(20); 
 			// 중복제거
 			for (int j = 0; j < i; j++) {
 				if (randQuestion[j] == randQuestion[i]) {
@@ -91,41 +193,15 @@ public class QuizFunction {
 				}
 			}
 		}
-		// 정렬
-		for (int i = 0; i < randQuestion.length; i++) {
-			for (int j = 0; j < randQuestion.length - 1; j++) {
-				if (randQuestion[j] > randQuestion[j + 1]) {
-					int temp = randQuestion[j];
-					randQuestion[j] = randQuestion[j + 1];
-					randQuestion[j + 1] = temp;
-				}
-			}
-		}
-		// 배열출력
-		for (int i = 0; i < randQuestion.length; i++) {
-			System.out.println((i + 1) + "번. " + quiz.quizArrs[randQuestion[i]]); // 문제
-			System.out.println(quiz.choiceArrs[randQuestion[i]]); // 보기
-			System.out.print("정답을 입력하시오 : ");
-			answer[i] = sc.nextInt();
-			if (answer[i] == quiz.answerSheetArrs[i]) {
-				System.out.println("정답입니다!");
-				System.out.println();
-			} else {
-				System.out.println("틀렸습니다...");
-				System.out.println("정답은 " + quiz.answerSheetArrs[i] + "번 입니다.");
-				System.out.println();
-			}
-		}
-		System.out.println("메인화면으로 돌아갑니다.");
-		System.out.println();
+		sort();
+		gamePlay();
 	}
 
+// oralce 4지선다
 	public void oracleChioceQuiz() {
-		Random rand = new Random();
-		Scanner sc = new Scanner(System.in);
-		Quiz quiz = new Quiz();
+		// oracle 4지선다 랜덤문제 인덱스값 5개 받기(20~39)
 		for (int i = 0; i < randQuestion.length; i++) {
-			randQuestion[i] = rand.nextInt(20) + 20; // oracle 4지선다 랜덤문제 인덱스값 5개 받기(20~39)
+			randQuestion[i] = rand.nextInt(20) + 20; 
 			for (int j = 0; j < i; j++) {
 				if (randQuestion[j] == randQuestion[i]) {
 					i--;
@@ -133,41 +209,15 @@ public class QuizFunction {
 				}
 			}
 		}
-		// 정렬
-		for (int i = 0; i < randQuestion.length; i++) {
-			for (int j = 0; j < randQuestion.length - 1; j++) {
-				if (randQuestion[j] > randQuestion[j + 1]) {
-					int temp = randQuestion[j];
-					randQuestion[j] = randQuestion[j + 1];
-					randQuestion[j + 1] = temp;
-				}
-			}
-		}
-		// 배열출력
-		for (int i = 0; i < randQuestion.length; i++) {
-			System.out.println((i + 1) + "번. " + quiz.quizArrs[randQuestion[i]]); // 문제
-			System.out.println(quiz.choiceArrs[randQuestion[i]]); // 보기
-			System.out.print("정답을 입력하시오 : ");
-			answer[i] = sc.nextInt();
-			if (answer[i] == quiz.answerSheetArrs[i]) {
-				System.out.println("정답입니다!");
-				System.out.println();
-			} else {
-				System.out.println("틀렸습니다...");
-				System.out.println("정답은 " + quiz.answerSheetArrs[i] + "번 입니다.");
-				System.out.println();
-			}
-		}
-		System.out.println("메인화면으로 돌아갑니다.");
-		System.out.println();
+		sort();
+		gamePlay();
 	}
 
+// java 4지선다
 	public void javaChioceQuiz() {
-		Random rand = new Random();
-		Scanner sc = new Scanner(System.in);
-		Quiz quiz = new Quiz();
+		// java 4지선다 랜덤문제 인덱스값 5개 받기(40~59)
 		for (int i = 0; i < randQuestion.length; i++) {
-			randQuestion[i] = rand.nextInt(20) + 40; // java 4지선다 랜덤문제 인덱스값 5개 받기(40~59)
+			randQuestion[i] = rand.nextInt(20) + 40; 
 			for (int j = 0; j < i; j++) {
 				if (randQuestion[j] == randQuestion[i]) {
 					i--;
@@ -175,41 +225,15 @@ public class QuizFunction {
 				}
 			}
 		}
-		// 정렬
-		for (int i = 0; i < randQuestion.length; i++) {
-			for (int j = 0; j < randQuestion.length - 1; j++) {
-				if (randQuestion[j] > randQuestion[j + 1]) {
-					int temp = randQuestion[j];
-					randQuestion[j] = randQuestion[j + 1];
-					randQuestion[j + 1] = temp;
-				}
-			}
-		}
-		// 배열출력
-		for (int i = 0; i < randQuestion.length; i++) {
-			System.out.println((i + 1) + "번. " + quiz.quizArrs[randQuestion[i]]); // 문제
-			System.out.println(quiz.choiceArrs[randQuestion[i]]); // 보기
-			System.out.print("정답을 입력하시오 : ");
-			answer[i] = sc.nextInt();
-			if (answer[i] == quiz.answerSheetArrs[i]) {
-				System.out.println("정답입니다!");
-				System.out.println();
-			} else {
-				System.out.println("틀렸습니다...");
-				System.out.println("정답은 " + quiz.answerSheetArrs[i] + "번 입니다.");
-				System.out.println();
-			}
-		}
-		System.out.println("메인화면으로 돌아갑니다.");
-		System.out.println();
+		sort();
+		gamePlay();
 	}
 
+// html OX
 	public void htmlYNQuiz() {
-		Random rand = new Random();
-		Scanner sc = new Scanner(System.in);
-		Quiz quiz = new Quiz();
+		// oracle O/X 랜덤문제 인덱스값 5개 받기(60~79)
 		for (int i = 0; i < randQuestion.length; i++) {
-			randQuestion[i] = rand.nextInt(20) + 60; // oracle O/X 랜덤문제 인덱스값 5개 받기(60~79)
+			randQuestion[i] = rand.nextInt(20) + 60; 
 			for (int j = 0; j < i; j++) {
 				if (randQuestion[j] == randQuestion[i]) {
 					i--;
@@ -217,41 +241,15 @@ public class QuizFunction {
 				}
 			}
 		}
-		// 정렬
-		for (int i = 0; i < randQuestion.length; i++) {
-			for (int j = 0; j < randQuestion.length - 1; j++) {
-				if (randQuestion[j] > randQuestion[j + 1]) {
-					int temp = randQuestion[j];
-					randQuestion[j] = randQuestion[j + 1];
-					randQuestion[j + 1] = temp;
-				}
-			}
-		}
-		// 배열출력
-		for (int i = 0; i < randQuestion.length; i++) {
-			System.out.println((i + 1) + "번. " + quiz.quizArrs[randQuestion[i]]); // 문제
-			System.out.println("1.O 2.X");
-			System.out.print("정답을 입력하시오 : ");
-			answer[i] = sc.nextInt();
-			if (answer[i] == quiz.answerSheetArrs[i]) {
-				System.out.println("정답입니다!");
-				System.out.println();
-			} else {
-				System.out.println("틀렸습니다...");
-				System.out.println("정답은 " + quiz.answerSheetArrs[i] + "번 입니다.");
-				System.out.println();
-			}
-		}
-		System.out.println("메인화면으로 돌아갑니다.");
-		System.out.println();
+		sort();
+		gamePlay();
 	}
 
+// oracle OX
 	public void oracleYNQuiz() {
-		Random rand = new Random();
-		Scanner sc = new Scanner(System.in);
-		Quiz quiz = new Quiz();
+		// oracle O/X 랜덤문제 인덱스값 5개 받기(80~99)
 		for (int i = 0; i < randQuestion.length; i++) {
-			randQuestion[i] = rand.nextInt(20) + 80; // oracle O/X 랜덤문제 인덱스값 5개 받기(80~99)
+			randQuestion[i] = rand.nextInt(20) + 80; 
 			for (int j = 0; j < i; j++) {
 				if (randQuestion[j] == randQuestion[i]) {
 					i--;
@@ -259,41 +257,15 @@ public class QuizFunction {
 				}
 			}
 		}
-		// 정렬
-		for (int i = 0; i < randQuestion.length; i++) {
-			for (int j = 0; j < randQuestion.length - 1; j++) {
-				if (randQuestion[j] > randQuestion[j + 1]) {
-					int temp = randQuestion[j];
-					randQuestion[j] = randQuestion[j + 1];
-					randQuestion[j + 1] = temp;
-				}
-			}
-		}
-		// 배열출력
-		for (int i = 0; i < randQuestion.length; i++) {
-			System.out.println((i + 1) + "번. " + quiz.quizArrs[randQuestion[i]]); // 문제
-			System.out.println("1.O 2.X");
-			System.out.print("정답을 입력하시오 : ");
-			answer[i] = sc.nextInt();
-			if (answer[i] == quiz.answerSheetArrs[i]) {
-				System.out.println("정답입니다!");
-				System.out.println();
-			} else {
-				System.out.println("틀렸습니다...");
-				System.out.println("정답은 " + quiz.answerSheetArrs[i] + "번 입니다.");
-				System.out.println();
-			}
-		}
-		System.out.println("메인화면으로 돌아갑니다.");
-		System.out.println();
+		sort();
+		gamePlay();
 	}
 
+// java OX
 	public void javaYNQuiz() {
-		Random rand = new Random();
-		Scanner sc = new Scanner(System.in);
-		Quiz quiz = new Quiz();
+		// oracle O/X 랜덤문제 인덱스값 5개 받기(100~119)
 		for (int i = 0; i < randQuestion.length; i++) {
-			randQuestion[i] = rand.nextInt(20) + 100; // oracle O/X 랜덤문제 인덱스값 5개 받기(100~119)
+			randQuestion[i] = rand.nextInt(20) + 100; 
 			for (int j = 0; j < i; j++) {
 				if (randQuestion[j] == randQuestion[i]) {
 					i--;
@@ -301,7 +273,12 @@ public class QuizFunction {
 				}
 			}
 		}
-		// 정렬
+		sort();
+		gamePlay();
+	}
+
+	// 정렬
+	public void sort() {
 		for (int i = 0; i < randQuestion.length; i++) {
 			for (int j = 0; j < randQuestion.length - 1; j++) {
 				if (randQuestion[j] > randQuestion[j + 1]) {
@@ -311,18 +288,22 @@ public class QuizFunction {
 				}
 			}
 		}
-		// 배열출력
+	}
+
+	// 배열출력
+	public void gamePlay() {
 		for (int i = 0; i < randQuestion.length; i++) {
-			System.out.println((i + 1) + "번. " + quiz.quizArrs[randQuestion[i]]); // 문제
-			System.out.println("1.O 2.X");
+			Quiz2 quizList = qList.get(randQuestion[i]);
+			System.out.printf("%d번. %s\n", i + 1, quizList.getQuizContent());
+			System.out.printf("%s\n", quizList.getChoiceArrs());
 			System.out.print("정답을 입력하시오 : ");
 			answer[i] = sc.nextInt();
-			if (answer[i] == quiz.answerSheetArrs[i]) {
+			if (quizList.getAnswerSheet() == answer[i]) {
 				System.out.println("정답입니다!");
 				System.out.println();
 			} else {
 				System.out.println("틀렸습니다...");
-				System.out.println("정답은 " + quiz.answerSheetArrs[i] + "번 입니다.");
+				System.out.printf("정답은 %d번 입니다.\n", quizList.getAnswerSheet());
 				System.out.println();
 			}
 		}
