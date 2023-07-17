cd32fe2fab9c906b281346ffd8e17799afe8a405
LeeJoonHan <134674322+LetDevOwl@users.noreply.github.com>
2023-07-17 12:47:56 +0900

    [ADD] 23/07/17 고오급 스윙 컴포넌트 part2
    1. JOptionPane 클래스, 자바스크립트의 alert, prompt, confirm과 같이 메시지 출력, 메시지 입력,
    Yes/No 선택을 할 수 있게함.
    2. JFileChooser 클래스, 첨부파일이 가능하도록 해줌.
    3. JColorChooser 클래스, 색깔이 가능하도록 해줌.
    -> 2, 3 모두 ActionListener 사용함.
    4. Audio를 출력할 수 있도록 함. -> LineListener을 사용하여 음악이 끝날 때 동작하도록 할 수 있음.

--- src/com/kh/day15/swing/component/Exam_JMenuBar.java
+++ src/com/kh/day15/swing/component/Exam_JMenuBar.java
@@ -5,33 +5,37 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
-public class Exam_JMenuBar extends JFrame {
+public class Exam_JMenuBar extends JFrame{
+
 	public Exam_JMenuBar() {
-		setTitle("JButton 이미지 예제");
+		
+		setTitle("메뉴 만들기 예제");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		createMenu();
-		setSize(250, 220);
+		setSize(250, 200);
 		setVisible(true);
 	}
 	
 	private void createMenu() {
-		JMenuBar bar = new JMenuBar();
-		JMenu screenMenu = new JMenu("screen"); 
+		JMenuBar bar = new JMenuBar();			// 메뉴바
+		JMenu screenMenu = new JMenu("screen");	// 메뉴 (screen)
 		
-		screenMenu.add(new JMenuItem("Load"));
+		screenMenu.add(new JMenuItem("Load"));	// 메뉴 아이템
 		screenMenu.add(new JMenuItem("Hide"));
-		screenMenu.add(new JMenuItem("Project"));
-		screenMenu.add(new JMenuItem("Run"));
+		screenMenu.add(new JMenuItem("ReShow"));
+		screenMenu.add(new JMenuItem("Exit"));
 		
 		bar.add(screenMenu);
-		bar.add(new JMenuItem("Edit"));
-		bar.add(new JMenuItem("Source"));
-		bar.add(new JMenuItem("Project"));
-		bar.add(new JMenuItem("Run"));
+		bar.add(new JMenu("Edit"));
+		bar.add(new JMenu("Source"));
+		bar.add(new JMenu("Project"));
+		bar.add(new JMenu("Run"));
 		
-		setJMenuBar(bar);
+		setJMenuBar(bar);	// 프레임에 추가
 	}
+	
 	public static void main(String[] args) {
-		new Exam_JMenuBar();
+		new Exam_JMenuBar();		
 	}
+
 }
