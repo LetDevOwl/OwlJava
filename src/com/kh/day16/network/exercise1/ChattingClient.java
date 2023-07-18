1c486593ab269fb9c59117212578e79d7414a0c7
LeeJoonHan <134674322+LetDevOwl@users.noreply.github.com>
2023-07-18 17:20:24 +0900

    [FIX] 23/07/18 ClientServer 파일명 변경
    1. ClientServer 파일명 변경

--- src/com/kh/day16/network/exercise1/ChattingClient.java
+++ src/com/kh/day16/network/exercise1/ChattingClient.java
@@ -7,8 +7,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class ClientServer {
-
+public class ChattingClient {
 	public static void main(String[] args) {
 		String address = "127.0.0.1";
 		int port = 5298;
